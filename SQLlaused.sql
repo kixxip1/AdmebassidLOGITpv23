7c8bf666bb0e5af11ee548b43302100bb4d62161
kixxip1 <surninamilana53@gmail.com>
2025-04-07 11:09:33 +0300

    tablet SQL

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -4,3 +4,21 @@
 --Server Name: (localdb) \mssqllocaldb 
 -- Authentification 2 tüüpi:
 --1. Windows Auth - localdb admini igused
+
+CREATE DATABASE surninaLOGITpv23;
+USE surninaLOGITpv23
+
+--tabeli loomine
+--identity(1,1) - ise täidab tabeli 1,2,3,.....
+CREATE TABle inimene(
+inimeneID int Primary Key identity (1,1),
+nimi varchar(50) unique,
+sysnniaeg date,
+telefon char(12),
+pikkus decimal(5,2),
+opilaskodu bit
+);
+SELECT * FROM inimene;
+
+--table kustutamiine 
+DROP table inimene;
