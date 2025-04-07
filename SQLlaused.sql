6458f9b5764f8440db5880b1a9902017d25e987e
kixxip1 <surninamilana53@gmail.com>
2025-04-07 11:40:16 +0300

    SQLlaused.sql

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -22,3 +22,20 @@ SELECT * FROM inimene;
 
 --table kustutamiine 
 DROP table inimene;
+
+
+--admete lisamine
+--DDL -data definition language
+--DML -data manipulation language
+
+INSERT INTO inimene(nimi synniaeg ,telefon ,pikkus, opilaskogu)
+VALUES
+'mari spik', '2028-11-24', '1234567', 90.5, 0);
+'Matt irl', '2008-3-54', '2568952', 90.5, 0);
+'Mket orl', '2011-1-23', '2568978', 90.5, 0);
+'Mmaks subl', '2023-12-33', '2555952', 90.5, 0);
+;
+SELECT * FROM inimene;
+--kustuta id=3
+DELETE FROM inimene
+WHERE inimene
